7f4357197d43cac5f1b77e9c596746dfce506fae
Toni Matkovic <toni.matkovic0@gmail.com>
2023-02-21 16:35:13 +0100

    Migrated Modal.tsx

--- Migrating_To_Typescript/src/Modal.tsx
+++ Migrating_To_Typescript/src/Modal.tsx
@@ -1,18 +1,25 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, MutableRefObject, ReactElement } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = ({ children }) => {
-  const elRef = useRef(null);
+const Modal = ({ children }: { children: ReactElement }) => {
+  const elRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
   if (!elRef.current) {
     elRef.current = document.createElement("div");
   }
 
   useEffect(() => {
     const modalRoot = document.getElementById("modal");
+    if (!modalRoot || !elRef.current) {
+      return;
+    }
     modalRoot.appendChild(elRef.current);
 
     // anything you return from useEffect if run when component unmounts from the dom, like using componentWillUnmount() lifecycle hook
-    return () => modalRoot.removeChild(elRef.current); // used ti remove something froom DOM, removing event listeners, timers/intervals
+    return () => {
+      if (elRef.current) {
+        modalRoot.removeChild(elRef.current);
+      } // used to remove something froom DOM, removing event listeners, timers/intervals
+    };
   }, []);
 
   return createPortal(<div>{children}</div>, elRef.current); // div wrapper is not needed, it's just for CSS purposes
